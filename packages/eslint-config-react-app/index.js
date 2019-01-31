62a4d1c520b19f06c10afe7900b62f7ddedc2edc
Luke Askew <lukeaskew@gmail.com>
2019-01-31 16:11:07 -0700

    Initial commit

---
+++ packages/eslint-config-react-app/index.js
@@ -0,0 +1,426 @@
+const restrictedGlobals = require('confusing-browser-globals');
+
+module.exports = {
+  extends: 'eslint:recommended',
+
+  root: true,
+
+  parser: 'babel-eslint',
+
+  plugins: ['import', 'jsx-a11y', 'react'],
+
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true
+  },
+
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+
+  rules: {
+    // http://eslint.org/docs/rules/
+    'array-bracket-spacing': ['warn', 'never'],
+    'array-callback-return': 'warn',
+    'arrow-body-style': ['warn', 'as-needed', {
+      requireReturnForObjectLiteral: false,
+    }],
+    'arrow-parens': ['warn', 'as-needed', {
+      requireForBlockBody: true,
+    }],
+    'arrow-spacing': ['warn', { before: true, after: true }],
+    'block-spacing': ['warn', 'always'],
+    'brace-style': ['warn', '1tbs', { allowSingleLine: true }],
+    'comma-dangle': [
+      'warn',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'always-multiline'
+      }
+    ],
+    'comma-spacing': ['warn', { before: false, after: true }],
+    'comma-style': [
+      'error',
+      'last',
+      {
+        exceptions: {
+          ArrayExpression: false,
+          ArrayPattern: false,
+          ArrowFunctionExpression: false,
+          CallExpression: false,
+          FunctionDeclaration: false,
+          FunctionExpression: false,
+          ImportDeclaration: false,
+          ObjectExpression: false,
+          ObjectPattern: false,
+          VariableDeclaration: false,
+          NewExpression: false
+        }
+      }
+    ],
+    'computed-property-spacing': ['warn', 'never'],
+    'constructor-super': 'error',
+    curly: ['warn', 'all'],
+    'default-case': ['warn', { commentPattern: '^no default$' }],
+    'dot-location': ['warn', 'property'],
+    'eol-last': ['warn', 'always'],
+    eqeqeq: ['warn', 'smart'],
+    'for-direction': 'warn',
+    'func-call-spacing': ['warn', 'never'],
+    'getter-return': 'warn',
+    indent: [
+      'warn',
+      2,
+      {
+        SwitchCase: 1,
+        VariableDeclarator: 1,
+        outerIIFEBody: 1,
+        // MemberExpression: null,
+        FunctionDeclaration: {
+          parameters: 1,
+          body: 1
+        },
+        FunctionExpression: {
+          parameters: 1,
+          body: 1
+        },
+        CallExpression: {
+          arguments: 1
+        },
+        ArrayExpression: 1,
+        ObjectExpression: 1,
+        ImportDeclaration: 1,
+        flatTernaryExpressions: false,
+        // list derived from https://github.com/benjamn/ast-types/blob/HEAD/def/jsx.js
+        ignoredNodes: [
+          'JSXElement',
+          'JSXElement > *',
+          'JSXAttribute',
+          'JSXIdentifier',
+          'JSXNamespacedName',
+          'JSXMemberExpression',
+          'JSXSpreadAttribute',
+          'JSXExpressionContainer',
+          'JSXOpeningElement',
+          'JSXClosingElement',
+          'JSXText',
+          'JSXEmptyExpression',
+          'JSXSpreadChild'
+        ],
+        ignoreComments: false
+      }
+    ],
+    'key-spacing': ['warn', { beforeColon: false, afterColon: true }],
+    'keyword-spacing': [
+      'warn',
+      {
+        before: true,
+        after: true,
+        overrides: {
+          return: { after: true },
+          throw: { after: true },
+          case: { after: true }
+        }
+      }
+    ],
+    'linebreak-style': ['warn', 'unix'],
+    'lines-between-class-members': [
+      'warn',
+      'always',
+      { exceptAfterSingleLine: false }
+    ],
+    'max-len': [
+      'warn',
+      100,
+      2,
+      {
+        ignoreUrls: true,
+        ignoreComments: false,
+        ignoreRegExpLiterals: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true
+      }
+    ],
+    'new-cap': [
+      'warn',
+      {
+        newIsCap: true,
+        newIsCapExceptions: [],
+        capIsNew: false,
+        capIsNewExceptions: ['Immutable.Map', 'Immutable.Set', 'Immutable.List']
+      }
+    ],
+    'new-parens': 'warn',
+    'newline-per-chained-call': 'warn',
+    'no-array-constructor': 'warn',
+    'no-await-in-loop': 'warn',
+    'no-caller': 'warn',
+    'no-compare-neg-zero': 'warn',
+    'no-cond-assign': ['warn', 'except-parens'],
+    'no-const-assign': 'warn',
+    'no-continue': 'warn',
+    'no-control-regex': 'warn',
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    'no-delete-var': 'warn',
+    'no-dupe-args': 'warn',
+    'no-dupe-class-members': 'warn',
+    'no-dupe-keys': 'warn',
+    'no-duplicate-case': 'warn',
+    'no-else-return': ['warn', { allowElseIf: false }],
+    'no-empty': 'warn',
+    'no-empty-character-class': 'warn',
+    'no-empty-function': [
+      'warn',
+      {
+        allow: ['arrowFunctions', 'functions', 'methods']
+      }
+    ],
+    'no-empty-pattern': 'warn',
+    'no-eval': 'warn',
+    'no-ex-assign': 'warn',
+    'no-extend-native': 'warn',
+    'no-extra-bind': 'warn',
+    'no-extra-label': 'warn',
+    'no-extra-semi': 'warn',
+    'no-fallthrough': 'warn',
+    'no-func-assign': 'warn',
+    'no-implied-eval': 'warn',
+    'no-invalid-regexp': 'warn',
+    'no-irregular-whitespace': 'warn',
+    'no-iterator': 'warn',
+    'no-label-var': 'warn',
+    'no-labels': ['warn', { allowLoop: true, allowSwitch: false }],
+    'no-lone-blocks': 'warn',
+    'no-lonely-if': 'warn',
+    'no-loop-func': 'warn',
+    'no-mixed-operators': [
+      'warn',
+      {
+        groups: [
+          ['&', '|', '^', '~', '<<', '>>', '>>>'],
+          ['==', '!=', '===', '!==', '>', '>=', '<', '<='],
+          ['&&', '||'],
+          ['in', 'instanceof']
+        ],
+        allowSamePrecedence: false
+      }
+    ],
+    'no-mixed-spaces-and-tabs': 'warn',
+    'no-multi-str': 'warn',
+    'no-multiple-empty-lines': ['warn', { max: 2, maxEOF: 0 }],
+    'no-native-reassign': 'warn',
+    'no-negated-in-lhs': 'warn',
+    'no-new-func': 'warn',
+    'no-new-object': 'warn',
+    'no-new-symbol': 'warn',
+    'no-new-wrappers': 'warn',
+    'no-obj-calls': 'warn',
+    'no-octal': 'warn',
+    'no-octal-escape': 'warn',
+    'no-redeclare': 'warn',
+    'no-regex-spaces': 'warn',
+    'no-restricted-properties': [
+      'error',
+      {
+        object: 'require',
+        property: 'ensure',
+        message:
+          'Please use import() instead. More info: https://facebook.github.io/create-react-app/docs/code-splitting'
+      },
+      {
+        object: 'System',
+        property: 'import',
+        message:
+          'Please use import() instead. More info: https://facebook.github.io/create-react-app/docs/code-splitting'
+      }
+    ],
+    'no-restricted-syntax': ['warn', 'WithStatement'],
+    'no-script-url': 'warn',
+    'no-self-assign': 'warn',
+    'no-self-compare': 'warn',
+    'no-sequences': 'warn',
+    'no-shadow-restricted-names': 'warn',
+    'no-sparse-arrays': 'warn',
+    'no-tabs': 'warn',
+    'no-template-curly-in-string': 'warn',
+    'no-this-before-super': 'warn',
+    'no-throw-literal': 'warn',
+    'no-trailing-spaces': 'warn',
+    'no-undef': 'error',
+    'no-restricted-globals': ['error'].concat(restrictedGlobals),
+    'no-return-await': 'warn',
+    'no-unexpected-multiline': 'warn',
+    'no-unneeded-ternary': ['warn', { defaultAssignment: false }],
+    'no-unreachable': 'warn',
+    'no-unused-expressions': [
+      'error',
+      {
+        allowShortCircuit: true,
+        allowTernary: true,
+        allowTaggedTemplates: true
+      }
+    ],
+    'no-unused-labels': 'warn',
+    'no-unused-vars': [
+      'warn',
+      {
+        args: 'none',
+        ignoreRestSiblings: true
+      }
+    ],
+    'no-use-before-define': [
+      'warn',
+      {
+        functions: false,
+        classes: false,
+        variables: false
+      }
+    ],
+    'no-useless-computed-key': 'warn',
+    'no-useless-concat': 'warn',
+    'no-useless-constructor': 'warn',
+    'no-useless-escape': 'warn',
+    'no-useless-rename': [
+      'warn',
+      {
+        ignoreDestructuring: false,
+        ignoreImport: false,
+        ignoreExport: false
+      }
+    ],
+    'no-with': 'warn',
+    'no-whitespace-before-property': 'warn',
+    'object-curly-newline': ['warn', { consistent: true }],
+    'object-curly-spacing': ['warn', 'always'],
+    'object-property-newline': 'warn',
+    'one-var': ['warn', 'never'],
+    'one-var-declaration-per-line': ['warn', 'always'],
+    'operator-assignment': ['warn', 'always'],
+    'quote-props': [
+      'warn',
+      'as-needed',
+      { keywords: false, unnecessary: true, numbers: false }
+    ],
+    quotes: ['warn', 'single', { avoidEscape: true }],
+    'require-yield': 'warn',
+    'rest-spread-spacing': ['warn', 'never'],
+    semi: ['warn', 'always'],
+    'semi-spacing': ['warn', { before: false, after: true }],
+    'semi-style': ['warn', 'last'],
+    'space-before-blocks': 'warn',
+    'space-before-function-paren': [
+      'warn',
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always'
+      }
+    ],
+    'space-in-parens': ['warn', 'never'],
+    'space-infix-ops': 'warn',
+    'space-unary-ops': [
+      'warn',
+      {
+        words: true,
+        nonwords: false,
+        overrides: {}
+      }
+    ],
+    'spaced-comment': [
+      'warn',
+      'always',
+      {
+        line: {
+          exceptions: ['-', '+'],
+          markers: ['=', '!'] // space here to support sprockets directives
+        },
+        block: {
+          exceptions: ['-', '+'],
+          markers: ['=', '!'], // space here to support sprockets directives
+          balanced: true
+        }
+      }
+    ],
+    'switch-colon-spacing': ['warn', { after: true, before: false }],
+    strict: ['warn', 'never'],
+    'template-tag-spacing': ['warn', 'never'],
+    'unicode-bom': ['warn', 'never'],
+    'use-isnan': 'warn',
+    'valid-typeof': 'warn',
+
+    // https://github.com/benmosher/eslint-plugin-import/tree/master/docs/rules
+    'import/first': 'error',
+    'import/no-amd': 'error',
+    'import/no-webpack-loader-syntax': 'error',
+
+    // https://github.com/yannickcr/eslint-plugin-react/tree/master/docs/rules
+    'react/forbid-foreign-prop-types': ['warn', { allowInPropTypes: true }],
+    'react/jsx-no-comment-textnodes': 'warn',
+    'react/jsx-no-duplicate-props': ['warn', { ignoreCase: true }],
+    'react/jsx-no-target-blank': 'warn',
+    'react/jsx-no-undef': 'error',
+    'react/jsx-pascal-case': [
+      'warn',
+      {
+        allowAllCaps: true,
+        ignore: []
+      }
+    ],
+    'react/jsx-uses-react': 'warn',
+    'react/jsx-uses-vars': 'warn',
+    'react/no-danger-with-children': 'warn',
+    'react/no-direct-mutation-state': 'warn',
+    'react/no-is-mounted': 'warn',
+    'react/no-typos': 'error',
+    'react/react-in-jsx-scope': 'error',
+    'react/require-render-return': 'error',
+    'react/style-prop-object': 'warn',
+    'react/jsx-closing-bracket-location': 'warn',
+    'react/jsx-first-prop-new-line': ['warn', 'multiline'],
+    'react/jsx-indent-props': ['warn', 2],
+    'react/jsx-max-props-per-line': 'warn',
+    'react/prop-types': 'warn',
+
+    // https://github.com/evcohen/eslint-plugin-jsx-a11y/tree/master/docs/rules
+    'jsx-a11y/accessible-emoji': 'warn',
+    'jsx-a11y/alt-text': 'warn',
+    'jsx-a11y/anchor-has-content': 'warn',
+    'jsx-a11y/anchor-is-valid': [
+      'warn',
+      {
+        aspects: ['noHref', 'invalidHref']
+      }
+    ],
+    'jsx-a11y/aria-activedescendant-has-tabindex': 'warn',
+    'jsx-a11y/aria-props': 'warn',
+    'jsx-a11y/aria-proptypes': 'warn',
+    'jsx-a11y/aria-role': 'warn',
+    'jsx-a11y/aria-unsupported-elements': 'warn',
+    'jsx-a11y/heading-has-content': 'warn',
+    'jsx-a11y/iframe-has-title': 'warn',
+    'jsx-a11y/img-redundant-alt': 'warn',
+    'jsx-a11y/no-access-key': 'warn',
+    'jsx-a11y/no-distracting-elements': 'warn',
+    'jsx-a11y/no-redundant-roles': 'warn',
+    'jsx-a11y/role-has-required-aria-props': 'warn',
+    'jsx-a11y/role-supports-aria-props': 'warn',
+    'jsx-a11y/scope': 'warn',
+  }
+};
